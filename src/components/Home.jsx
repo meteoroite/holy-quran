332bc9db9ed74f839545c288be5ea67dc7f9e014
meteoroite <90157302+meteoroite@users.noreply.github.com>
2023-09-25 00:38:35 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
       <img src={IMG} style={{ width: 'fit-content', height: 'fit-content', borderRadius: '10%' }} alt="" />
       <div className='cont2'>
         {/* Replace <a> tags with <Link> components */}
-        <Link to='/read' className='link'>
+        <Link to='https://holy-quran-1.vercel.app/read' className='link'>
           Read the Quran
         </Link>
-        <Link to='/listen' className='link'>
+        <Link to='https://holy-quran-1.vercel.app/listen' className='link'>
           Listen to the Quran
         </Link>
       </div>

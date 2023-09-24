83de129d788582d27cf426ef27dc13c4d230f90d
meteoroite <90157302+meteoroite@users.noreply.github.com>
2023-09-24 22:18:50 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import IMG from '../assets/holyquran.png'
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import IMG from '../assets/holyquran.png';
 
 const Home = () => {
   return (
     <div className='cont'>
-            <img src={IMG} style={{width: 'fit-content', height: 'fit-content', borderRadius: '10%'}} alt="" />
-        <div className='cont2'>
-        <a href='/read'  className='link'>
-            Read the Quran
-        </a>
-        <a href='/listen' className='link'>
-            Listen to the Quran
-        </a>
-        </div>
+      <img src={IMG} style={{ width: 'fit-content', height: 'fit-content', borderRadius: '10%' }} alt="" />
+      <div className='cont2'>
+        {/* Replace <a> tags with <Link> components */}
+        <Link to='/read' className='link'>
+          Read the Quran
+        </Link>
+        <Link to='/listen' className='link'>
+          Listen to the Quran
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
